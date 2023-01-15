2dd8d5ccff437db5145e47c7ee5cec92f3637f6d
DFB IT <itdesk@dfbnigeria.org.ng>
2023-01-15 22:19:30 +0100

    Finish Modal App

---
+++ js/vanillaJS/modal-v01/modal.js
@@ -0,0 +1,15 @@
+const openBtn = document.querySelector("#wrapper button");
+const closeBtn = document.querySelector("#modal-close button");
+const modalContainer = document.querySelector(".modal-container");
+
+openBtn.addEventListener("click", openModal);
+
+closeBtn.addEventListener("click", closeModal);
+
+function openModal(e) {
+  modalContainer.style.display = "flex";
+}
+
+function closeModal() {
+  modalContainer.style.display = "none";
+}
